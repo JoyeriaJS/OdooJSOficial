9d0efaae5ecea7e3b9683425fb50e549bbe8539d
unknown <dante.h@hotmail.es>
2025-09-03 12:47:58 -0400

    cambios validacion de telefono obligatorio10

--- custom_addons/joyeria_reparaciones/models/reparacion.py
+++ custom_addons/joyeria_reparaciones/models/reparacion.py
@@ -135,7 +135,7 @@ class Reparacion(models.Model):
     ], string='Estado', default='presupuesto', tracking=True, required=True, store=True, readonly=False)
 
 
-    clave_autenticacion_manual = fields.Char(string='QR de quien recibe', tracking=False)
+    clave_autenticacion_manual = fields.Char(string='QR de quien recibe', tracking=True, required=True)
 
     # NUEVOS CAMPOS
     metal_utilizado = fields.Selection([
