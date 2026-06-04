861e413eb03a5af61e2b68f2d8c69dc3e02884f2
unknown <dante.h@hotmail.es>
2026-06-04 10:08:26 -0400

    cambios1eraversiondepdv57

--- custom_addons/joyeria_reparaciones/__manifest__.py
+++ custom_addons/joyeria_reparaciones/__manifest__.py
@@ -11,12 +11,12 @@
         'views/reparacion_js.xml',
         #'data/ir_sequence_data.xml',
         #'data/joyeria_data.xml',
-        #'report/report.xml',  # ← este es el que importa
-        #'report/report_qrcode_template.xml',
+        'report/report.xml',  # ← este es el que importa
+        'report/report_qrcode_template.xml',
         #'report/reporte_responsable_report.xml',
         #'static/src/reparacion_form.js',
-        #'report/reporte_reparaciones_responsable.xml',
-        #'views/vendedora_qr_views.xml',
+        'report/reporte_reparaciones_responsable.xml',
+        'views/vendedora_qr_views.xml',
          #'static/src/xml/pos_rma_line.xml',
         'views/vendedora_views.xml',
         'views/trabajadores_views.xml',
